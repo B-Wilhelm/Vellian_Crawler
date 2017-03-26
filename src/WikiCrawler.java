aeae23815ef69439ab43c92794362413340998b5
B_Wilhelm <bwilhelm@iastate.edu>
2017-03-25 19:29:29 -0500

    Java doc prep work for the first two methods done

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -11,7 +11,11 @@ public class WikiCrawler {
 	private String seedUrl, fileName;
 	private int max;
 	
-	
+	/*
+	 * @param seedURL String representing relative address of the Seed URL
+	 * @param max Integer primitive representing the max number of pages to crawl
+	 * @param fileName String representing the name of the file that the graph will be written to
+	 */
 	public WikiCrawler(String seedUrl, int max, String fileName) {
 		this.seedUrl = seedUrl;
 		this.max = max;
