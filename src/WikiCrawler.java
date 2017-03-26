67ebbe8bd7654771e7f8d3fd62f65c25acf2facc
B_Wilhelm <bwilhelm@iastate.edu>
2017-03-25 19:08:46 -0500

    starting work on first class, wikicrawler

---
+++ src/WikiCrawler.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Brett Wilhelm
+ * @author Zach Johnson
+ *
+ */
+
+public class WikiCrawler {
+	private String seedUrl, fileName;
+	private int max;
+	
+	public WikiCrawler(String seedUrl, int max, String fileName) {
+		this.seedUrl = seedUrl;
+		this.max = max;
+		this.fileName = fileName;
+	}
+	
+	/*
+	 * @param doc String that contains 
+	 */
+	private ArrayList<String> extractLinks(String doc) {
+		
+		
+		return null;
+	}
+}
