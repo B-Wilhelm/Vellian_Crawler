ace8e92e8863977ca7d416cb4ca1a8358b918d62
Zach <Zachj@iastate.edu>
2017-04-03 17:35:23 -0500

    implemented first pass at graph using an adjacency list

--- src/AdjacencyList.java
+++ src/AdjacencyList.java
@@ -1,16 +1,45 @@
 
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class AdjacencyList {
 	private final int V; 
 	private int E;
-	private List<Node> adj;
+	private List<Node>[] adj;
 	private int[] indegree;
 	
 	public AdjacencyList(int v) {
+		if(v <= 0) {throw new IllegalArgumentException();}
+		
 		V = v;
 		this.E = 0;
 		indegree = new int[V];
+		adj = (List<Node>[]) new List[V];
+		for(int i = 0;i < V; i++) {
+			adj[i] = new List<Node>();
+		}
+		
 	}
+	
+	public void addEdge(int v, Node to) {
+		isVertexValid(v);
+		isVertexValid(to.getLoc());
+		adj[v].add(to);
+		indegree[to.getLoc()]++;
+		E++;
+	}
+	
+	public int getV() { return V;}
+	public int getE() { return E;}
+	
+	public int outdegree(int v) {
+		isVertexValid(v);
+		return adj[v].size();
+	}
+	
+	private void isVertexValid(int v) {
+		if (v < 0 || v>= V) throw new IllegalArgumentException();
+	}
+	
 }
--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,14 @@
 //Node class for our graph
-public class Node {
-	String name;
+public class Node{
+	private String name;//name of the link
+	private int loc;// place in the array
+
+	public Node(String n, int l){
+		this.name = n;
+		this.loc = l;
+	}
+	
+	public String getName(){ return name;}
+	public int getLoc(){ return loc;}
 	
 }
