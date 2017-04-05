9f98098f5a67fc04c263fdade9de91e59198b217
Zach <Zachj@iastate.edu>
2017-04-04 23:55:54 -0500

    working on SCC helper methods

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -16,7 +16,7 @@ import java.util.Stack;
 public class GraphProcessor {
 	private int V;
 	private AdjacencyList graph;
-	private Map<String, Boolean> isTraveled;
+	private Map<String, Boolean> isTraveled, undiscovered;
 	private LinkedList<String> neighbors;
 	private ArrayList<String> path = new ArrayList<String>();
 	private Stack<String> s = new Stack<String>();
@@ -29,6 +29,7 @@ public class GraphProcessor {
 		for(String key : graph.getKeys()) {
 			isTraveled.put(key, false);
 		}
+		undiscovered.putAll(isTraveled);
 	}
 	
 	public int outDegree(String v) {
@@ -54,6 +55,8 @@ public class GraphProcessor {
 	public ArrayList<String> bfsPath(String u, String v) {
 		path = new ArrayList<String>();
 		s = new Stack<String>();
+		isTraveled.clear();
+		isTraveled.putAll(undiscovered);
 		
 		BFSPathFinder(u,v);
 		
@@ -62,7 +65,7 @@ public class GraphProcessor {
 		}
 		return path;
 	}
-	//private helper methods
+	//private helper methods used
 	private void BFSPathFinder(String u, String v) {
 		setIsTraveled(u);
 		s.push(u);
@@ -85,10 +88,67 @@ public class GraphProcessor {
 			return;
 		}	
 	}
+	private AdjacencyList getTranspose() {
+		AdjacencyList tmp = new AdjacencyList(V);
+		
+		for(String key : graph.getKeys()){
+			neighbors = graph.getNeighbors(key);
+			it = neighbors.iterator();
+			while(it.hasNext()) {
+				tmp.adj.get(key).add(it.next());
+			}
+		}
+		
+		return tmp;
+	}
+	
+	private void fillOrder(String v, Map<String, Boolean> visited, Stack<String> s) {
+		setIsTraveled(v);
+		
+		neighbors = graph.getNeighbors(v);
+		it = neighbors.iterator();
+		while(it.hasNext()) {
+			String cur = it.next();
+			if(visited.get(cur) == false) {
+				fillOrder(cur, visited, s);
+			}
+		}
+		
+		s.push(new String(v));
+	}
+	
 	
 	private void setIsTraveled(String v)
 	{
 		isTraveled.replace(v, true);
+	}
+	
+	private void computeSCCs() {
+		
+		isTraveled.clear();
+		isTraveled.putAll(undiscovered);
+		Stack<String> s1 = new Stack<String>();
+		
+		isTraveled.clear();
+		isTraveled.putAll(undiscovered);
+		
+		for(String key : graph.getKeys()){
+			if(isTraveled.get(key) == false) {
+				fillOrder(key, isTraveled, s1);
+			}
+		}
+		
+		AdjacencyList reverse = getTranspose();
+		
+		isTraveled.clear();
+		isTraveled.putAll(undiscovered);
+		
+		while(s1.isEmpty() == false) {
+			String v = s1.pop();
+			
+			//TODO
+		}
+		
 	}
 	
 }
