1190762b761bf77b1e8fdd2042408ca52ededd21
Zach <Zachj@iastate.edu>
2017-04-05 19:15:35 -0500

    sus

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -23,13 +23,13 @@ public class WikiCrawler {
 	private String seedUrl, fileName;
 	private int max;
 	private Scanner s, l;	// Scanner for entire source code, Scanner for individual links
-	private String source, scannedText, progSource;
+	private String source, input, progSource;
 	private static final String CONTAINS_CHECK = "/wiki/";
 	private static final String[] NOT_CONTAINED = {":", "#"};
 	private AdjacencyList graph; // the graph our crawler will create
 	private File f;
 	private Queue<String> test;	//needed for BFS change name from test
-	
+	private boolean toggle;
 	
 	/*
 	 * @param seedURL String representing relative address of the Seed URL
@@ -41,66 +41,64 @@ public class WikiCrawler {
 		this.max = max;
 		this.fileName = fileName;
 		
-		source = getPageSource(seedUrl);
-		list = extractLinks(source);
+		list = extractLinks(getPageSource(seedUrl));
+		crawl();
 	}
-	
 	
 	/*
 	 * @param doc String that represents the source code of an html page
 	 */
-	
 	public ArrayList<String> extractLinks(String doc) {
-		ArrayList<String> newList = new ArrayList<String>();
-		scannedText = "";
+		addToGraph(doc);
 		
-		s = new Scanner(doc);	// Scanner for whole html source code
-		s.useDelimiter("<p>|<P>");
-		
-		if(s.hasNext()) { s.next(); }// Skips to just after first instance of <p> or <P>
-		
-		s.useDelimiter("href=\"|\"");
-		
-		while(s.hasNext()) {
-			scannedText = s.next();
-			
-			if((scannedText.toLowerCase()).contains(CONTAINS_CHECK) && !((scannedText.toLowerCase()).contains(NOT_CONTAINED[0])) && !((scannedText.toLowerCase()).contains(NOT_CONTAINED[1])) && (scannedText.charAt(1)=='w')) {	// Ensures properly formatted links get through
-				if(((newList.size() + list.size()) < max) && !(newList.contains(scannedText)) && !(list.contains(scannedText)) && !(scannedText.equals(seedUrl))) {	// Ensures links aren't duplicates or self-loops and stops collecting at "max" value
-					System.out.println(seedUrl + " : " + scannedText);
-					newList.add(scannedText);
-				}
-				else {
-					s.close();
-					return newList;
-				}
-			}
-		}
-		
-		s.close();
-		return newList;
+		return null;
 	}
-	
 	
 	public void crawl() {
 		graph = new AdjacencyList(max);
-		Iterator<String> iter = list.iterator();
 		
-		while(iter.hasNext()) {
-			if((list.size() < max)) {
-				list.addAll(extractLinks(iter.next()));
-			}
-		}
-		
-		
+//		for(int i = 0; i<list.size() && list.size()<max; i++) {
+//			seedUrl = list.get(i).getEnd();
+//			temp = extractLinks(getPageSource(seedUrl));
+//			list.addAll(temp);
+//		}
+//	
+//		toggle = true;
+//	
+//		for(int i = 0; i < list.size(); i++) {
+//			temp = extractLinks(getPageSource(seedUrl));
+//			System.out.println(list.addAll(temp));
+//		}
 	}
-	
 	
 	/////////////////////////////////////////
 	
+	private void addToGraph(String doc) {
+		graph = new AdjacencyList(max);
+		ArrayList<String> newList = new ArrayList<String>();
+		input = "";
+		s = new Scanner(doc);	// Scanner for whole html source code
+		s.useDelimiter("<p>|<P>");
+		if(s.hasNext()) { s.next(); }	// Skips to just after first instance of <p> or <P>
+		s.useDelimiter("href=\"|\"");
+		
+		while(s.hasNext()) {
+			input = s.next();
+			
+			if((input.toLowerCase()).contains(CONTAINS_CHECK) && !((input.toLowerCase()).contains(NOT_CONTAINED[0])) && !((input.toLowerCase()).contains(NOT_CONTAINED[1])) && (input.charAt(1)=='w')) {	// Ensures properly formatted links get through
+				if(!toggle && graph.size()<max) {
+					
+				}
+				else {
+					
+				}
+			}
+		}
+		s.close();
+	}
 	
 	private String getPageSource(String urlS) {	// Takes in relative URL
 		progSource = "";
-		
 		URL url;
 	    InputStream input = null;
 	    BufferedReader br = null;
@@ -110,15 +108,12 @@ public class WikiCrawler {
 	        url = new URL(BASE_URL + urlS);
 	        input = url.openStream();  // throws an IOException
 	        br = new BufferedReader(new InputStreamReader(input));
-
 	        if ((line = br.readLine()) != null) {
 	        	progSource += line;
 	        }
-	        
 	        while ((line = br.readLine()) != null) {
 	            progSource += "\n" + line;
 	        }
-	        
 	        return progSource;
 	        
 	    } catch (MalformedURLException m) {
@@ -129,18 +124,20 @@ public class WikiCrawler {
 	        try {
 	            if (input != null) {
 	            	input.close();
-//	            	br.close();
 	            }
 	        } catch (IOException i) {
 	            // all good
 	        }
 	    }
-		
 		return "";
 	}
 	
 	public String getSource() {	// May make private
 		return source;
+	}
+	
+	public ArrayList<String> getList() {
+		return list;
 	}
 	
 	private void writeToFile() {
